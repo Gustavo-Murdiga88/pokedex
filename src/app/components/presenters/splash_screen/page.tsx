f229e14e53b5f8ca8a93aa65f615a84a4f8a98b3
Gustavo Murdiga <gustavo.murdiga@guaranisistemas.com.br>
2023-07-25 14:13:22 -0300

    initial commit

---
+++ src/app/components/presenters/splash_screen/page.tsx
@@ -0,0 +1,15 @@
+import Img from "next/image";
+
+export function SplashScreen() {
+	return (
+		<section className="flex h-screen w-screen items-center justify-center">
+			<Img
+				className="animate-bounce"
+				src="/assets/icon.svg"
+				height={167}
+				width={167}
+				alt="poke ball"
+			/>
+		</section>
+	);
+}
