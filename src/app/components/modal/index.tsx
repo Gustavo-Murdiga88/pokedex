fd4ef811a91f07cbc905cbe7ba8fce40f3ea5471
Gustavo Murdiga <gustavo.murdiga@guaranisistemas.com.br>
2023-07-26 17:36:14 -0300

    feat: create a button on modal pokemon

--- src/app/components/modal/index.tsx
+++ src/app/components/modal/index.tsx
@@ -1,11 +1,14 @@
 import Img from "next/image";
 
+import { Button } from "./components/button";
+
+import pokebool from "../../assets/icons/pokeballEclipse.png";
 import { Tag } from "../card";
 import { Heart } from "lucide-react";
 
 export function Modal() {
 	return (
-		<div className="relative max-h-[789px] w-[524px]">
+		<div className="relative mx-auto max-h-[789px] w-[524px] drop-shadow-card">
 			<div
 				className="h-[789px] w-[524px]"
 				style={{
@@ -13,7 +16,7 @@ export function Modal() {
 					WebkitMaskImage: "url(/assets/rectangle.svg)",
 				}}
 			>
-				<header className="flex h-[240px] justify-end bg-fire">
+				<header className="relative flex h-[240px] justify-end bg-fire">
 					<div className="mb-4 mr-[39px] mt-auto flex h-[105px] w-[223px] flex-col items-start gap-1">
 						<span className="text-md font-regular text-gray50">#004</span>
 						<span className="mb-2 text-5xl font-extraBold leading-none text-gray50">
@@ -21,10 +24,41 @@ export function Modal() {
 						</span>
 						<Tag />
 					</div>
+					<Img
+						className="absolute bottom-[-10px] left-[20px] blur-[6px]"
+						src={pokebool}
+						width={181}
+						height={181}
+						alt="pokeboll"
+					/>
 				</header>
-
-				<Heart className="absolute right-[17px] top-[50px]" size={40} />
+				<button className="absolute right-[17px] top-[50px] rounded-md p-0 leading-none">
+					<Heart size={40} />
+				</button>
 			</div>
+
+			<main className=" absolute top-[240px] flex h-[561px] flex-col bg-gray50">
+				<div className="mb-[20px] mt-[50px] flex justify-between px-[40px]">
+					<Button active={false}>About</Button>
+					<Button active>Stats</Button>
+					<Button active={false}>Evolution</Button>
+					<Button active={false}>Moves</Button>
+				</div>
+				<div className="flex flex-1 flex-col gap-8 bg-gray100 px-16 pb-[53px] pt-[40px]">
+					<span>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nam
+						eum magnam ea modi asperiores sit harum, et eveniet. Totam dolore
+					</span>
+					<span>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nam
+						eum magnam ea modi asperiores sit harum, et eveniet. Totam dolore
+					</span>
+					<span>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nam
+						eum magnam ea modi asperiores sit harum, et eveniet. Totam dolore
+					</span>
+				</div>
+			</main>
 			<Img
 				id="pokemon"
 				className="absolute left-[20px] top-[30px]"
--- tailwind.config.js
+++ tailwind.config.js
@@ -98,6 +98,10 @@ module.exports = {
 		backgroundImage: {
 			pokeBoll: "url(/src/app/assets/icons/pokeball.svg)",
 		},
+
+		dropShadow: {
+			card: "2px -4px 8px rgba(0, 0, 0, 0.25)",
+		},
 	},
 	plugins: [],
 };
