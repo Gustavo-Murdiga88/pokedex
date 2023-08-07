75adc1dd6f0aeb1858c844bd728c7acc2a348642
Gustavo Murdiga <gustavo.murdiga@guaranisistemas.com.br>
2023-08-07 11:58:04 -0300

    refactor error on scroller

--- src/app/components/modal/components/moves/index.tsx
+++ src/app/components/modal/components/moves/index.tsx
@@ -13,13 +13,16 @@ interface IMovesProps {
 
 function Component({ moves, tag }: IMovesProps) {
 	return (
-		<ul
+		<div
+			id="moves"
+			role="button"
+			tabIndex={-1}
+			className="fixed top-0 h-[348px] w-full touch-pan-y snap-center overflow-auto bp-2:h-[400px]"
 			onTouchStart={(e) => {
 				e.stopPropagation();
+
+				e.currentTarget.focus();
 				document.getElementById("main")!.style.overflow = "hidden";
-				setTimeout(function () {
-					e.currentTarget.style.overflow = "scroll";
-				}, 100);
 			}}
 			onTouchEnd={(e) => {
 				e.stopPropagation();
@@ -28,30 +31,31 @@ function Component({ moves, tag }: IMovesProps) {
 			onTouchMove={(e) => {
 				e.stopPropagation();
 			}}
-			id="moves"
-			className="fixed top-0 h-[348px] w-full touch-pan-y snap-center overflow-auto bp-2:h-[400px]"
 		>
-			{moves.map(({ name }) => {
-				const move = name.replace("-", " ");
-				return (
-					<li
-						key={move}
-						className="flex items-center justify-between border-b border-b-gray200 py-2 pl-1 pt-3 capitalize"
-					>
-						<span className="text-sm font-medium text-gray500 dark:text-gray50 bp-1:text-lg">
-							{name}
-						</span>
-						<Img
-							className="h-[18px] w-[18px] bp-1:h-[30px] bp-1:w-[30px]"
-							width={30}
-							height={30}
-							alt="medal"
-							src={`/assets/tags/${tag}.svg`}
-						/>
-					</li>
-				);
-			})}
-		</ul>
+			<ul>
+				{moves.map(({ name }) => {
+					const move = name.replace("-", " ");
+					return (
+						<li
+							role="menuitem"
+							key={move}
+							className="flex items-center justify-between border-b border-b-gray200 py-2 pl-1 pt-3 capitalize"
+						>
+							<span className="text-sm font-medium text-gray500 dark:text-gray50 bp-1:text-lg">
+								{name}
+							</span>
+							<Img
+								className="h-[18px] w-[18px] bp-1:h-[30px] bp-1:w-[30px]"
+								width={30}
+								height={30}
+								alt="medal"
+								src={`/assets/tags/${tag}.svg`}
+							/>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
 	);
 }
 
